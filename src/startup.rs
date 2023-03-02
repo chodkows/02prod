9f4f239c1be2cf4f572e624fb2acaf87cf746cf9
chodkows <chodkows@gmail.com>
2023-03-02 08:40:04 +0100

    Change folder structure, add sqlx

---
+++ src/startup.rs
@@ -0,0 +1,14 @@
+use actix_web::{web, HttpServer, App, dev::Server};
+use crate::routes::{health_check, subscribe};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscription", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
